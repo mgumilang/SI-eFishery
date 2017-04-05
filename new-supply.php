33523ba1f469117ecf72b1c1f4a62fcfbe6e7c7a
mgumilang <muhammadgumilang11@gmail.com>
2017-04-05 11:28:15 +0700

    Add css new-supply

--- new-supply.php
+++ new-supply.php
@@ -5,6 +5,7 @@
 		<meta charset = "UTF-8">
 		<meta name = "viewport" content = "width = device-width, initial-scale = 1">
         <link rel="stylesheet" href="css/bootstrap.min.css">
+        <link rel="stylesheet" href="css/new-supply.css">
         <div class="date"></div>
     </header>
     <body>
